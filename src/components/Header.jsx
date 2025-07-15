3501829409a7f39e843c2abf06623dc876c26b58
SabirinMohamudAdan <sabrinadan327@gmail.com>
2025-07-15 05:35:59 -0700

    last update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -125,7 +125,7 @@ function Header() {
               SAPRI <span className='text-amber-500'>N</span>AA */}
 
 <Link to="/" className="font-bold text-white text-4xl">
-  CU<span className="text-amber-500">NOO</span>
+Resto<span className="text-amber-500">ra</span>
               <p className="text-[15px] ml-2 flex items-center gap-2">
                 <svg className="w-5 h-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
