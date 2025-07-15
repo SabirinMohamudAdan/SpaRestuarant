1fbfe3478fcde81950105d3c32dfe4600cce33aa
SabirinMohamudAdan <sabrinadan327@gmail.com>
2025-07-15 01:04:06 -0700

    new

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,3 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-// import { BrowserRouter } from 'react-router-dom'
-
-// createRoot(document.getElementById('root')).render(
-// //  <BrowserRouter>
-// //     <App />
-// //  </BrowserRouter>
-// <React.StrictMode>
-// <BrowserRouter>
-//   <App />
-// </BrowserRouter>
-// </React.StrictMode>
- 
-// )
-
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
